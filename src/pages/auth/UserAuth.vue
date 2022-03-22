6ecb6965502dafad2e12888a1c49f5f6633ff5ec
Sankha Bhattacharjee <sankhab1999@gmail.com>
2022-03-22 21:15:18 +0530

    Adding authentication, login and signup

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <base-dialog :show="!!error" title="An error occurred!" @close="handleError"> {{ error }}</base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-mail</label>
+          <input name="email" id="email" type="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input
+            name="password"
+            id="password"
+            type="password"
+            v-model.trim="password"
+          />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter correct email/password (must be atleast 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="toggleAuthMode">{{
+          toggleButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
+</template>
+
+<script>
+import BaseDialog from '../../components/ui/BaseDialog.vue';
+export default {
+  components: { BaseDialog },
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+      isLoading: false,
+      error: null,
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Login';
+      } else {
+        return 'Signup';
+      }
+    },
+    toggleButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Signup instead';
+      } else {
+        return 'Login instead';
+      }
+    },
+  },
+  methods: {
+    async submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      this.isLoading = true;
+      const actionPayload = {
+            email: this.email,
+            password: this.password,
+          };
+      try {
+        if (this.mode === 'login') {
+          await this.$store.dispatch('login',actionPayload);
+        } else {
+          console.log('submit');
+          await this.$store.dispatch('signup', actionPayload);
+        }
+      } catch (err) {
+        this.error = err.message || 'Failed to authenticate, try again later.';
+      }
+
+      this.isLoading = false;
+    },
+    toggleAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+      console.log(this.mode);
+    },
+    handleError(){
+      this.error = null;
+    }
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>
---
+++ src/store/modules/auth/getters.js
@@ -0,0 +1,8 @@
+export default {
+    getUserId(state){
+      return state.userId;
+    },
+    getToken(state){
+        return state.token;
+    }
+  };
